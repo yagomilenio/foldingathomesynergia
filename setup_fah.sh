4cc9bcce324cb419a1885d9e52da5e9b35cd949a
yagomilenio <34864685+yagomilenio@users.noreply.github.com>
2026-04-01 17:34:08 +0200

    Update setup_fah.sh

--- setup_fah.sh
+++ setup_fah.sh
@@ -5,4 +5,3 @@ git clone https://github.com/foldingathome/fah-client-bastet
 export CBANG_HOME=$PWD/cbang
 scons -C cbang
 scons -C fah-client-bastet
-echo "$(cat /proc/sys/kernel/random/uuid | tr -d '-')" > /etc/machine-id
