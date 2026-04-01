d5d0c8aaec662220b3607ab08d745c5481e2e027
yagomilenio <34864685+yagomilenio@users.noreply.github.com>
2026-04-01 19:54:49 +0200

    Update setup_fah.sh

--- setup_fah.sh
+++ setup_fah.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+chmod +x control_output.sh
 git clone https://github.com/cauldrondevelopmentllc/cbang
 git clone https://github.com/foldingathome/fah-client-bastet
 export CBANG_HOME=$PWD/cbang
